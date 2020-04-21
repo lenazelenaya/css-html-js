e6ba9490bd98355168e685554f5d6c695d2ecd98
lenazelly <lena.zelenaya@gmail.com>
2020-04-21 23:21:36 +0300

    day-night events by button-click and EventListener

--- Third test/script.js
+++ Third test/script.js
@@ -5,6 +5,27 @@
     // box.onclick = function() {
     //     this.style.backgroundColor = "yellow";
     // };
+    var changeColor = function() {
+
+        if (this.id === 'day') {
+            doc.body.className = "day";
+        } else if (this.id === "night") {
+            doc.body.className = "night";
+        }
+    };
+
+    var sayHi = function() {
+        alert(this.textContent + "!");
+    }
+
+    var buttons = doc.getElementsByTagName('button');
+
+    for (let index = 0; index < buttons.length; index++) {
+        buttons[index].addEventListener('click', changeColor, false);
+        buttons[index].addEventListener('click', sayHi, false);
+    };
+
+
 
 
 
