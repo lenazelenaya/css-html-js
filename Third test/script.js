22c496edcfe403cba17cbc72b43dc9ce69ce3913
lenazelly <lena.zelenaya@gmail.com>
2020-04-22 10:09:29 +0300

    Added delegation of fun

--- Third test/script.js
+++ Third test/script.js
@@ -4,19 +4,13 @@
     var changeColor = function(e) {
         eventsObj.preventDefault(e);
 
-        var elem = eventsObj.getTarget(e);
+        var elem = eventsObj.getTarget(e),
+            colorData = elem.getAttribute("data-color");
 
-
-        if (elem.id === 'day') {
-            doc.body.className = "day";
-        } else if (elem.id === "night") {
-            doc.body.className = "night";
+        if (colorData) {
+            doc.body.className = colorData;
         }
     };
 
-    var buttons = doc.getElementsByTagName('button');
-
-    for (let index = 0; index < buttons.length; index++) {
-        eventsObj.addEvent(buttons[index], 'click', changeColor);
-    };
+    eventsObj.addEvent(doc, 'click', changeColor);
 })();
